a07b11528f5b57ae088183eef4dd1f9a88034e81
Alex Elder <alex@handwritten.io>
2014-02-14 17:15:53 +0000

    Updated gulpfile with new tasks and package.json with new dev deps.

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,17 @@
 var gulp   = require('gulp'),
     jshint = require('gulp-jshint'),
     watch  = require('gulp-watch'),
+    mocha  = require('gulp-mocha'),
     yuidoc = require('gulp-yuidoc');
 
 gulp.task('lint', function () {
     gulp.src(['lib/*.js', 'test/**/*.js'])
         .pipe(jshint())
         .pipe(jshint.reporter('default'));
+});
+
+gulp.task('test', function () {
+    gulp.src('./test/*.js').pipe(mocha());
 });
 
 gulp.task('docs', function () {
@@ -21,6 +26,13 @@ gulp.task('docs', function () {
 gulp.task('watch', function () {
     gulp.watch(['lib/*.js', 'test/**/*.js'], function () {
         gulp.run('lint');
+    });
+});
+
+gulp.task('continuous', function () {
+
+    gulp.watch(['lib/*.js', 'test/**/*.js'], function () {
+        gulp.run(['lint', 'test']);
     });
 });
 
